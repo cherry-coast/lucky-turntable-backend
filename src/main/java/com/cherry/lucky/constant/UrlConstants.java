2f2c9698ffbb2996f24bc54fe516a1d2fce977b9
cherry-coast <cherry-coast@163.com>
2023-05-22 15:07:57 +0800

    新增部分请求地址

--- src/main/java/com/cherry/lucky/constant/UrlConstants.java
+++ src/main/java/com/cherry/lucky/constant/UrlConstants.java
@@ -11,4 +11,14 @@ public class UrlConstants {
 
     public static final String LOGIN_URL = "/api/v1/wechat/login";
 
+    public static final String BACK_USER_LOGIN_URL = "/api/v1/user/login";
+
+    public static final String BACK_USER_REGISTER_URL = "/api/v1/user/register";
+
+    public static final String FAVICON_URL = "/favicon.ico";
+
+    public static final String SWAGGER_URL = "/swagger-resources";
+
+    public static final String API_URL = "/v3/api-docs";
+
 }
